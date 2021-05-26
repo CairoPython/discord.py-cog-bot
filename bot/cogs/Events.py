e7a58f552c34298e80cb82738a2c9c327d1f91f0
Kai Amos <83473782+CairoPython@users.noreply.github.com>
2021-05-26 12:16:05 -0700

    nsfw commands

--- bot/cogs/Events.py
+++ bot/cogs/Events.py
@@ -1,7 +1,9 @@
 import discord
 from discord.ext import commands
+from better_profanity import profanity
 
-class Random(commands.Cog):
+
+class Events(commands.Cog):
   
   def __init__(self, bot):
     self.bot=bot
@@ -9,14 +11,37 @@ class Random(commands.Cog):
     
   #listeners
   @commands.Cog.listener
-  async def on_message(self, ctx):
+  async def on_message(self, message, ctx):
     if message.author.has_roles('Muted'):
       await ctx.delete_message()
+      
+  @commands.Cog.listener
+  async def on_message(self, ctx, message):
+      
+  #commands
+  @commands.command
+  async def nsfw_on(self, ctx):
+    if ctx.message.author.guild_permissions.administrator:
+      nsfw = True
+      await ctx.send("NSFW message filters off!")
+    else:
+      await ctx.send("Sorry, you can't use that command!")
+    return nsfw
+   
+    @commands.command
+  async def nsfw_off(self, ctx):
+    if ctx.message.author.guild_permissions.administrator:
+      nsfw = False
+      await ctx.send("NSFW message filters on!")
+    else:
+      await ctx.send("Sorry, you can't use that command!")
+    return nsfw  
+   
   
   
     
  
 def setup(bot):
-  bot.add_cog(Random(bot))
+  bot.add_cog(Events(bot))
     
   
